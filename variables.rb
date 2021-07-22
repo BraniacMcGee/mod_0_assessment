15ef36b94780fabc56f12006dc0f370fbfcfc95c
Cole Henson <colehenson95@gmail.com>
2021-07-22 10:47:11 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "Brad Pitt"
+is_active = true
+famous_person = "Dwayne Johnson"
+num_3 = 3
+num_9 = 9
+sum = num_3 + num_9
+animals = ["shark", "elephant", "leopard"]
+animals.shift()
+p famous_person.length
