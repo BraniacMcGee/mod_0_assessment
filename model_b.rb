48b90dcaffc724dfc4463cad454cfa8ecc125d63
Cole Henson <colehenson95@gmail.com>
2021-07-22 11:11:00 -0600

    add pet hash

---
+++ model_b.rb
@@ -0,0 +1,11 @@
+pet = {
+  name: "Cooper",
+  breed: "German Shepard",
+  height_inches: 24,
+  weight_lbs: 40,
+  is_cute: true,
+  is_happy: true,
+  colors: ["brown", "black"],
+  favorite_foods: ["chicken", "pork", "ice"]
+}
+p pet
