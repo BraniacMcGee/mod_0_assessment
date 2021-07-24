3dfaed48877b63a6553a1141b663b6231f1e18bd
Cole Henson <colehenson95@gmail.com>
2021-07-24 11:23:01 -0600

    restructure file path

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+school = {
+  name: "Mountain Range",
+  city: "Westminster",
+  number_of_students: 2000,
+  football_team_wins: 4,
+  is_high_school: true,
+  is_fun: false,
+  subjects: ["math", "physics", "drama", "history"],
+  teachers_last_names: ["Simpson", "Schenck", "Kyde", "Buck"]
+}
---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+pet = {
+  name: "Cooper",
+  breed: "German Shepard",
+  height_inches: 24,
+  weight_lbs: 45,
+  is_cute: true,
+  is_happy: true,
+  colors: ["brown", "black"],
+  favorite_foods: ["chicken", "pork", "rice"]
+}
+p pet
